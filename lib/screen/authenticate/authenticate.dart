b58f18be9f5ef768ad5cc4b855cb49cd4a7ba46b
Mohamad-Haniff <haniffmpk2021@gmail.com>
2020-12-03 03:24:12 +0800

    first commit

---
+++ lib/screen/authenticate/authenticate.dart
@@ -0,0 +1,24 @@
+import 'package:firebase/screen/authenticate/sign_in.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase/screen/authenticate/register.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  bool showSignIn = true;
+  void toogleView() {
+    setState(() => showSignIn = !showSignIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showSignIn) {
+      return SignIn(toogleView: toogleView);
+    } else {
+      return Register(toogleView: toogleView);
+    }
+  }
+}
